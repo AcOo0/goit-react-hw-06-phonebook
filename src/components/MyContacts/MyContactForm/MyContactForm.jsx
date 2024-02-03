19f00fa524354db83040e54676b6390fb1fcf572
AcOo0 <acoo.012@gmail.com>
2024-02-03 15:07:02 +0200

    fix bugs

--- src/components/MyContacts/MyContactForm/MyContactForm.jsx
+++ src/components/MyContacts/MyContactForm/MyContactForm.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
+import { useSelector, useDispatch } from 'react-redux';
 import { nanoid } from "nanoid";
+
+import { getFilteredContacts } from '../../../redux/contacts/constants-selectors';
+import { addContact } from "../../../redux/contacts/contacts-slice";
 
 import styles from './my-contact-form.module.css'
 
@@ -10,8 +14,33 @@ const INITIAL_STATE = {
     number: ''
 };
 
-const MyContactForm = ({ onSubmit }) => {
+const MyContactForm = () => {
     const [state, setState] = useState({ ...INITIAL_STATE });
+    const contacts = useSelector(getFilteredContacts);
+    const dispatch = useDispatch();
+
+    const isDublicate = ({ name, number }) => {
+        const normalizedName = name.toLowerCase();
+        const normalizedNumber = number.trim();
+        
+        const dublicate = contacts.find(item => {
+            const normalizeCurrentName = item.name.toLowerCase();
+            const normalizeCurrentNumber = item.number.trim();
+            return (normalizeCurrentName === normalizedName || normalizeCurrentNumber === normalizedNumber)
+        })
+
+        return Boolean(dublicate);
+    };
+
+    const onAddContact = (data) => {
+        if (isDublicate(data)) {
+            alert(`${data.name} or Number: ${data.number} is already in contacts.`);
+            return;
+        }
+        
+        const action = addContact(data);
+        dispatch(action);
+    };
 
     const handleChange = ({ target }) => {
         const { name, value } = target;
@@ -23,8 +52,10 @@ const MyContactForm = ({ onSubmit }) => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        onSubmit({ ...state });
-        reset();
+        onAddContact({ ...state });
+        if (!isDublicate({ ...state })) {
+            reset();
+        }
     };
 
     const reset = () => {
@@ -41,11 +72,27 @@ const MyContactForm = ({ onSubmit }) => {
         <form onSubmit={handleSubmit} className={styles.form}>
             <div className={styles.formElements}>
                 <label htmlFor={contactNameId}>Name</label>
-                <input value={name} onChange={handleChange} id={contactNameId} name="name" required type="text" placeholder="Name" />
+                <input
+                    value={name}
+                    onChange={handleChange}
+                    id={contactNameId}
+                    name="name"
+                    required
+                    type="text"
+                    placeholder="Name" />
             </div>
             <div className={styles.formElements}>
                 <label htmlFor={contactNumberId}>Number</label>
-                <input value={number} onChange={handleChange} id={contactNumberId} name="number" required type="tel" placeholder="Number" /></div>
+                <input
+                    value={number}
+                    onChange={handleChange}
+                    id={contactNumberId}
+                    name="number"
+                    required
+                    pattern="^[+0-9\-\(\)\s]+$"
+                    type="tel"
+                    placeholder="Number" />
+            </div>
             <button className={styles.btn} type="submit">Add contact</button>
         </form>
     );
--- src/components/MyContacts/MyContacts.jsx
+++ src/components/MyContacts/MyContacts.jsx
@@ -1,55 +1,20 @@
-import { useSelector, useDispatch } from 'react-redux';
-
 import MyContactForm from './MyContactForm/MyContactForm';
 import MyContactList from './MyContactList/MyContactList';
 import Filter from './Filter/Filter';
 
-import { addContact, deleteContact } from '../../redux/contacts/contacts-slice';
-import { setFilter } from '../../redux/filter/filter-slice';
-import { getFilteredContacts } from '../../redux/contacts/constants-selectors';
+
 
 import styles from './my-contacts.module.css'
 
 const MyContacts = () => {
-    const contacts = useSelector(getFilteredContacts);
-    const dispatch = useDispatch();
-
-    const isDublicate = ({ name, number }) => {
-        const normalizedName = name.toLowerCase();
-        const normalizedNumber = number.trim();
-        
-        const dublicate = contacts.find(item => {
-            const normalizeCurrentName = item.name.toLowerCase();
-            const normalizeCurrentNumber = item.number.trim();
-            return (normalizeCurrentName === normalizedName || normalizeCurrentNumber === normalizedNumber)
-        })
-
-        return Boolean(dublicate);
-    };
-
-    const onAddContact = (data) => {        
-        if (isDublicate(data)) {
-            return alert(`${data.name} or Number: ${data.number} is already in contacts.`)
-        }
-        
-        const action = addContact(data);
-        dispatch(action);
-    };
-
-    const onDeleteContact = (id) => {
-        dispatch(deleteContact(id));
-    };
-
-    const changeFilter = ({ target }) => dispatch(setFilter(target.value));
-    
     return (
         <div className={styles.wrapper}>
             <h1>Phonebook</h1>
-            <MyContactForm onSubmit={onAddContact} />
+            <MyContactForm />
             <div className={styles.listwrapper}>
                 <h2>Contacts</h2>
-                <Filter onChange={changeFilter} />
-                <MyContactList items={contacts} deleteContact={onDeleteContact} />
+                <Filter />
+                <MyContactList />
             </div>
         </div>
     );
